284bea4cf1b0a3b5b3d884c593023948bca15ee7
Olamilekan <headofstate123@gmail.com>
2022-08-04 15:31:26 +0000

    add the json to the Employeee struct

--- main.go
+++ main.go
@@ -8,15 +8,15 @@ import (
 
 	"github.com/gofiber/fiber/v2"
 	"github.com/joho/godotenv"
-	"go.mongodb.org/mongo-driver/bson"
-	"go.mongodb.org/mongo-driver/bson/primitive"
+	// "go.mongodb.org/mongo-driver/bson"
+	// "go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
 type MongoInstance struct{
-	Client
-	Db
+	Client	*mongo.Client
+	Db		*mongo.Database
 }
 
 var mg MongoInstance
@@ -24,11 +24,11 @@ var mg MongoInstance
 // dbName := goDotEnvVariable("DBNAME")
 
 type Employee struct{
-	ID		string
-	Name	string
-	Salary	float64
-	Age		float64
-	Sex		string
+	ID		string	`json:"id, omitempty" bson:"_id, omitempty`
+	Name	string	`json:"name"`
+	Salary	float64	`json:"salary"`
+	Age		float64	`json:"age"`
+	Gender	string	`json:"gender"`
 }
 
 func Connect() error{
@@ -38,18 +38,21 @@ func Connect() error{
 	mongoURI := os.Getenv("MONG0URI") + dbName
 
 	client, err := mongo.NewClient(options.Client().ApplyURI(mongoURI))
-	context.WithTimeout(context.Background(), 25*time.Second)
+	ctx, cancel := context.WithTimeout(context.Background(), 25*time.Second)
 	defer cancel()
 
 	err = client.Connect(ctx)
-	db : client.Database(dbName)
+	db := client.Database(dbName)
 	
 	if err != nil {
 		return err
 	}
 
-	mg = MongoInstance{Client : client,
-	Db: }
+	mg = MongoInstance{
+		Client : client,
+		Db: db,
+	}
+	return nil
 }
 
 func cancel() {
@@ -62,7 +65,7 @@ func main(){
 		log.Fatal(err)
 	}
 	app := fiber.New()
-	app.Get("/employee", func(c *fiber.Ctx) error{})
+	// app.Get("/employee", func(c *fiber.Ctx) error{})
 	app.Post("/employee")
 	app.Put("/emplyee/:id")
 	app.Delete("emplyee/:id")
